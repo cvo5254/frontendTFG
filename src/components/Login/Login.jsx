261d351af44fe69a4d5eeb5a4210aed9cd69c017
cvo5254 <cvo5254@gmail.com>
2023-02-08 13:39:34 +0100

    Css del login acabado

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -11,10 +11,12 @@ const Login = () => {
 
     return (
     <div className='login-container'>
+    <h2>Login</h2>
     <form className='form-container' onSubmit={handleSubmit}>
       <label>
         Username:
         <input
+          className='username-input'
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
@@ -24,13 +26,14 @@ const Login = () => {
       <label>
         Password:
         <input
+          className='password-input'
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
       </label>
       <br />
-      <button type="submit">Login</button>
+      <button className='button-login' type="submit">Login</button>
     </form>
     </div>
     );
