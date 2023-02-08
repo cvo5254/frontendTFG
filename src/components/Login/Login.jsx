0942e57939fff5dc05be1f84a30fe9412e87f4bb
Cristobal Vidal Orquin <crvidorq@upv.edu.es>
2023-02-08 17:14:08 +0100

    Fetch configurado

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,42 +1,51 @@
-import React, { useState } from 'react';
-import "./Login.css"
+import React, { useState } from "react";
+import { login } from "../../services/login";
+import "./Login.css";
 
-const Login = () => { 
-    const [ username, setUsername] = useState('');
-    const [ password, setPassword] = useState('');
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let response = await login(username, password);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    return (
-    <div className='login-container'>
-    <h2>Login</h2>
-    <form className='form-container' onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
-          className='username-input'
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input
-          className='password-input'
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <br />
-      <button className='button-login' type="submit">Login</button>
-    </form>
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form className="form-container" onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            className="username-input"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            className="password-input"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button className="button-login" type="submit">
+          Login
+        </button>
+      </form>
     </div>
-    );
-}
+  );
+};
 
 export default Login;
--- src/services/login.js
+++ src/services/login.js
@@ -1,3 +1,10 @@
-async function login() {
-
+async function login(username, password) {
+  const respuesta = await fetch("http://localhost:8000/api/login/", {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({ username, password }),
+  });
+  return respuesta.json();
 }
+
+export { login };
