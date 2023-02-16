86942362a22423055f70864a6004202dad5db58e
cvo5254 <cvo5254@gmail.com>
2023-02-16 09:07:10 +0100

    Cambios a fetch login

--- src/services/login.js
+++ src/services/login.js
@@ -1,8 +1,8 @@
-async function login(username, password) {
-  const respuesta = await fetch("http://localhost:8000/api/login/", {
+async function login(email, password) {
+  const respuesta = await fetch("http://localhost:8000/api/token/", {
     method: "POST",
     headers: { "Content-type": "application/json" },
-    body: JSON.stringify({ username, password }),
+    body: JSON.stringify({ email, password }),
   });
   return respuesta.json();
 }
