e387f156a7d7de38c78ea581165fb5ece98776ec
Cristobal Vidal Orquin <crvidorq@upv.edu.es>
2023-05-15 11:38:44 +0200

    Tabla genérica hecha

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,25 +1,27 @@
 import { useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 
-const Table = () => {
-  const data = [
-    { name: "John", active: true },
-    { name: "Jane", active: false },
-    { name: "Bob", active: true },
-  ];
-  const [rows, setRows] = useState(
-    data.map((row, index) => ({ ...row, id: index + 1 }))
-  );
+const Table = ({ data, columns }) => {
+  const [rows, setRows] = useState(data);
 
-  const columns = Object.keys(data[0]).map((key) => ({
-    field: key,
-    headerName: key.toUpperCase(),
-    width: 200,
+  const renderCell = (params) => {
+    const { field, value } = params;
+
+    if (field === "Acciones") {
+      return value;
+    }
+
+    return String(value);
+  };
+
+  const formattedColumns = columns.map((column) => ({
+    ...column,
+    renderCell: renderCell,
   }));
 
   return (
-    <div>
-      <DataGrid rows={rows} columns={columns} pageSize={5} />
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid rows={rows} columns={formattedColumns} pageSize={10} />
     </div>
   );
 };
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,9 +1,76 @@
 import Table from "../Table/Table";
+import { useState, useEffect } from "react";
+import { Button } from "@mui/material";
 
 const Users = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getUsers() {
+      try {
+        const response = await fetch(
+          "http://localhost:8000/api/obtener_usuarios/?is_active=False"
+        );
+        const data = await response.json();
+        const users = data.map((user, index) => ({
+          Id: index,
+          Email: user.email,
+          Estado: user.is_active,
+          Acciones: (
+            <>
+              <Button
+                variant="contained"
+                color="primary"
+                size="small"
+                onClick={() => handleActivate(user.id)}
+              >
+                Activar
+              </Button>
+              <Button
+                variant="contained"
+                color="secondary"
+                size="small"
+                onClick={() => handleDelete(user.id)}
+              >
+                Eliminar
+              </Button>
+            </>
+          ),
+        }));
+        setUsers(users);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getUsers();
+  }, []);
+
+  const handleActivate = (id) => {
+    // Lógica para activar usuario con el ID `id`
+    console.log("Activar usuario con el ID:", id);
+  };
+
+  const handleDelete = (id) => {
+    // Lógica para eliminar usuario con el ID `id`
+    console.log("Eliminar usuario con el ID:", id);
+  };
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  const columns = [
+    { field: "Id", headerName: "ID", width: 200 },
+    { field: "Email", headerName: "Email", width: 200 },
+    { field: "Estado", headerName: "Estado", width: 200 },
+    { field: "Acciones", headerName: "Acciones", width: 200 },
+  ];
+
   return (
     <div>
-      <Table></Table>
+      <Table data={users} columns={columns} />
     </div>
   );
 };
